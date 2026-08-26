7137bc21e0fa21a2d8168a9de91077fdef0d05f9
ahmedemadelsheikh-rgb <ahmed.emad.elsheikh@gmail.com>
2026-08-26 17:29:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
-const toggle=document.getElementById("servicesToggle");const submenu=document.getElementById("submenu");const arrow=document.getElementById("arrow");const semester=document.getElementById("semesterBtn");const result=document.getElementById("result");const go=document.getElementById("goBtn");toggle.addEventListener("click",()=>{submenu.classList.toggle("hidden");arrow.textContent=submenu.classList.contains("hidden")?"›":"⌄"});semester.addEventListener("click",()=>{document.querySelector(".card-title").scrollIntoView({behavior:"smooth"})});go.addEventListener("click",()=>{result.classList.remove("hidden");go.textContent="Go";});
+const toggle = document.getElementById("servicesToggle");
+const submenu = document.getElementById("submenu");
+const arrow = document.getElementById("arrow");
+
+const semester = document.getElementById("semesterBtn");
+const semesterPage = document.getElementById("semesterPage");
+
+const result = document.getElementById("result");
+const go = document.getElementById("goBtn");
+
+
+// My E-Services
+toggle.addEventListener("click", () => {
+
+  submenu.classList.toggle("hidden");
+
+  if (submenu.classList.contains("hidden")) {
+    arrow.textContent = "›";
+  } else {
+    arrow.textContent = "⌄";
+  }
+
+});
+
+
+// Semester Result
+semester.addEventListener("click", () => {
+
+  semesterPage.classList.remove("hidden");
+
+  semesterPage.scrollIntoView({
+    behavior: "smooth"
+  });
+
+});
+
+
+// Go button
+go.addEventListener("click", () => {
+
+  result.classList.remove("hidden");
+
+  go.textContent = "Go";
+
+});
